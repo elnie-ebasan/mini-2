cc2fee25646612e0875d4b2fec4301ec0bf6bee3
elnie-ebasan <eebasan43@gmail.com>
2023-02-03 15:59:45 +0800

    initialize mini-2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import logo from './mensahe-logo.jpg';
+// import imahe from './imahe.jpg';
 import './App.css';
 
-function App() {
+function EmailForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Email: ${email}`);
+    console.log(`Password: ${password}`);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
+   
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  
+          <form onSubmit={handleSubmit}>
+            <label>
+            Email:
+              <br />
+              <input type="email" value={email} onChange={handleEmailChange} />
+            </label>
+            <br />
+            <br/>
+      
+            <label>
+            Password:
+              <br />
+              
+              <input type="password" value={password} onChange={handlePasswordChange} />
+            </label>
+
+            <div className="signUp">
+            <p>Sign Up</p>
+            <p>Forgot Password</p>
+            </div>
+            <br />
+      
+            <button type="submit" className='submitButton'>Sign In</button>
+          </form>
       </header>
     </div>
   );
 }
 
-export default App;
+export default EmailForm;
